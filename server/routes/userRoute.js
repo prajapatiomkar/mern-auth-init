d115ab4eff34a19c34a46a9475ff6e7456df9ac8
Omkar Prajapati <72141037+prajapatiomkar@users.noreply.github.com>
2024-01-09 00:28:18 +0530

    added functionality to generate a jwt token and save in cookie.

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   userLoginController,
+  userLogoutController,
   userRegisterController,
 } from "../controllers/userController.js";
 import { registerUserValidation } from "../validations/user/user.validation.js";
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/register", registerUserValidation, userRegisterController);
 router.post("/login", userLoginController);
+router.delete("/logout", userLogoutController);
 
 export default router;
